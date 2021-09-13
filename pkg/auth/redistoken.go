4b8910124e9ed6fd60411c4bfd548514b1866c51
Philip Zhang <zhangxilai@doublechaintech.com>
2021-09-13 16:30:02 +0800

    change auth

--- pkg/auth/redistoken.go
+++ pkg/auth/redistoken.go
@@ -100,7 +100,7 @@ func (auth *RedisTokenAuthSetterVerifier) SetNewWorkConn(newWorkConnMsg *msg.New
 
 func (auth *RedisTokenAuthSetterVerifier) VerifyLogin(loginMsg *msg.Login) error {
 	if util.GetAuthKey(auth.token, loginMsg.Timestamp) != loginMsg.PrivilegeKey {
-		return fmt.Errorf("token in login doesn't match token from configuration by redis")
+		return fmt.Errorf("token in login doesn't match token from configuration by redis" + loginMsg.User)
 	}
 	return nil
 }
