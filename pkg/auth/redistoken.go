1bd724c2804f2eedaa5619a8556b075bd821b2b8
Philip Zhang <zhangxilai@doublechaintech.com>
2021-09-13 18:14:04 +0800

    change auth

--- pkg/auth/redistoken.go
+++ pkg/auth/redistoken.go
@@ -109,11 +109,6 @@ func (auth *RedisTokenAuthSetterVerifier) SetPing(pingMsg *msg.Ping) error {
 
 func (auth *RedisTokenAuthSetterVerifier) SetNewWorkConn(newWorkConnMsg *msg.NewWorkConn) error {
 
-	for key, value := range newWorkConnMsg.Metas {
-		fmt.Printf("%s = %s\n", key, value)
-
-	}
-
 	if !auth.AuthenticateNewWorkConns {
 		return nil
 	}
